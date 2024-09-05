64dc77e764b40fe4ac9b737c6b954410a692314d
hasanasadov <hasanaliasadov@gmail.com>
2024-09-06 01:52:08 +0400

    market place js css update

--- client/pages/marketplace/script.js
+++ client/pages/marketplace/script.js
@@ -1,6 +1,8 @@
 const nfts = document.querySelector("#nfts");
 const nftBottom = document.querySelector(".nft-bottom");
 const loadMoreBtn = document.querySelector("#load-more");
+const searchInput = document.querySelector("#search");
+
 function fillNfts(nftsData) {
     nftsData.forEach((nft) => {
         const nftElement = document.createElement("div");
@@ -10,7 +12,7 @@ function fillNfts(nftsData) {
                     <img src="../../../${nft.imgPath}" alt="">
                 </div>
                 <div class="nft-image-bottom">
-                    <h5>Space Walking</h5>
+                    <h5>${nft.name}</h5>
                     <div class="nft-image-bottom__bottom">
                         <img src="../../../${nft.creator.profileImgPath}" alt=""> ${nft.creator.name}
                     </div>
@@ -31,26 +33,55 @@ function fillNfts(nftsData) {
 }
 
 let skip = 0;
+let hasMore = true;
 async function getNfts(count = 0) {
     skip += count;
+
     const response = await fetch(`http://localhost:3000/api/nfts`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            "pageSize": 3, 
-            "skip": skip
+            pageSize: 3,
+            skip: skip,
         }),
     });
     const nftsData = await response.json();
+    hasMore = nftsData.hasMore;
+    console.log(nftsData);
     fillNfts(nftsData.nfts);
-    if(!nftsData.hasMore){return;} 
 }
 
 loadMoreBtn.addEventListener("click", () => {
     getNfts(3);
+    if (!hasMore) {
+        Toastify({
+            text: "This is no more items.",
+            style: {
+                background: "red",
+            },
+            duration: 1000,
+        }).showToast();
+        return;
+    }
 });
 
+searchInput.addEventListener("keyup", async (e) => {
+    const response = await fetch(`http://localhost:3000/api/nfts`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            "pageSize": "18",
+            "searchStr": e.target.value,
+        }),
+    });
+    const nftsData = await response.json();
+    nftBottom.innerHTML = "";
+    fillNfts(nftsData.nfts);
+    loadMoreBtn.style.display = "none";
+});
 
 getNfts();

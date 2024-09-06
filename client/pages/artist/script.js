a900a6e18a1694b75440a21e9bd97066f8b0306a
hasanasadov <hasanaliasadov@gmail.com>
2024-09-06 05:30:17 +0400

    creators page js get request done

--- client/pages/artist/script.js
+++ client/pages/artist/script.js
@@ -8,10 +8,55 @@ async function getCreatorInfo() {
     try {
         const response = await fetch(`http://localhost:3000/api/creators/${creatorId}`);
         const creator = await response.json();
-        console.log(creator);
+        return creator;
     } catch (error) {
-        console.log(error);
     }
 }
 
 getCreatorInfo();
+
+
+
+const nfts = document.querySelector("#creators");
+const creatorBottom = document.querySelector(".creator-bottom");
+let creator = getCreatorInfo();
+
+function fillCreators(creator) {
+    if (creator.nfts.length === 0) {
+        nfts.innerHTML = `
+            <div class="no-creators">
+                <h2>No NFTs found</h2>
+            </div>
+        `;
+    }
+    creator.nfts.forEach((data) => {
+        const creatorElement = document.createElement("div");
+        creatorElement.classList.add("creator-item");
+        creatorElement.innerHTML = `
+                <div class="creator-image">
+                    <img src="../../../${data.imgPath}" alt="">
+                </div>
+                <div class="creator-image-bottom">
+                    <h5>${data.name}</h5>
+                    <div class="creator-image-bottom__bottom">
+                        <img src="../../../${creator.profileImgPath}" alt=""> ${data.name}
+                    </div>
+                    <div class="creator-price">
+                        <div class="price-left">
+                            <p>Price</p>
+                            <span>${data.price.value} ${data.price.currency}</span>
+                        </div>
+                        <div class="price-right">
+                            <p>Highest Bid</p>
+                            <span>${data.highestBid.value} ${data.highestBid.currency}</span>
+                        </div>
+                    </div>
+                </div>
+        `;
+        creatorBottom.appendChild(creatorElement);
+    });
+}
+
+creator.then((data) => {
+    fillCreators(data);
+});
